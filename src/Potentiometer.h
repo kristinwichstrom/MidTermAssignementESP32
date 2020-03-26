9f11bf8b71dabfccdd321ee770a7fd6ad38127a6
kristinwichstrom <kristinwichstrom@gmail.com>
2020-03-26 12:52:43 +0100

    working on potentiometer

--- src/Potentiometer.h
+++ src/Potentiometer.h
@@ -1,40 +1,73 @@
 #include <Arduino.h>
 
-class Potentiometer {
+class Potentiometer{
     public:
-    int pinNumber;
-    int lastRead;
-    int sensitivity;
-    int color;
+        int pinNumber;
+        int lastRead;
+        int currentRead;
+        int sensitivity;
+        String color = "DEFAULT COLOR";
+    
 
-    int redVal = 0;   // Variables to store the values to send to the pins
-    int grnVal = 0;
-    int bluVal = 0;
+        Potentiometer(int pinNumber)
+        {
+            this->pinNumber = pinNumber;
+            pinMode(pinNumber, INPUT);
+            this->lastRead = this->readPotentiometer();
+        }
 
-Potentiometer (int pinNumber)
-{
- this -> pinNumber = pinNumber;   
- pinMode (pinNumber, INPUT);
- this->lastRead = this->readPotentiometer();
-}
-int readPotentiometer()
+        int readPotentiometer()
         {
             return analogRead(pinNumber);
         }
- void setSensitivity(int sensitivity)
+
+        void setSensitivity(int sensitivity)
         {
             this->sensitivity = sensitivity;
         }
-void setColor(int color)
-{
-    
-this->color = color;
-}
-    bool hasChanged()
+
+        bool hasChanged()
         {
+            Serial.println(abs(lastRead-analogRead(pinNumber)));
             return abs(lastRead-analogRead(pinNumber)) > sensitivity;
         }
+         void setColor(String message)
+       {
+           this -> color = message;
+       }
+       
+        void settingColor()
+        {
+            if (this->hasChanged())
+            {
+                currentRead = readPotentiometer();
 
-
-
+                if (currentRead <=1356 )
+                {
+                    setColor("color 255 0 0");
+                }
+                else if(currentRead > 1356 && currentRead <= 2712)
+                {
+                    setColor("color 0 255 0");
+                }
+                 else
+                {
+                    setColor("color 0 0 255");
+                }
+               
+              /*  int digiRead = digitalRead(this->pinNumber);
+               
+                if (this->hasChanged() && digiRead <= 1356)
+             {
+                    Serial.println(this->infoMessage);
+                }
+                else if (this->hasChanged() && digiRead > 1356)
+                {
+                Serial.println(this->infoMessage);
+                }
+                else {
+                    Serial.println(this->infoMessage);
+                }*/
+            }
+        }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,42 @@
 #include <Arduino.h>
+#include "WiFi.h"
+#include "AsyncUDP.h"
+#include "Button.h"
 #include <Potentiometer.h>
 
+
+const char * ssid = "Krisw";     //Insert your wifi name
+const char * password = "solbakk11"; //insert your wifi password
+
+int PIN_BUTTON = 23;
+Button button (PIN_BUTTON);
 Potentiometer potentiometer(13);
+AsyncUDP udp;
 
 void setup() {
-  potentiometer.setSensitivity(50);
-  potentiometer.setInfo("INFORMATION!!!");
-  potentiometer.setWarning("WARNING!!!!!");
-
-  Serial.begin(9600);
-
-  delay(2000);
-  Serial.println(potentiometer.readPotentiometer());
-
+ 
+  potentiometer.setSensitivity(100);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.begin(9600);
 
 }
 
 void loop() {
-  potentiometer.loop();
-  
-  if (potentiometer.hasChanged()) {
-    Serial.println(potentiometer.readPotentiometer());
+
+if (button.notPressed())
+  {
+    Serial.print ("");
+  } 
+  else
+  {
+    udp.broadcastTo("init 9 9", 7000);
   }
-  
+  //delay (300);
+  potentiometer.settingColor();
+  if (potentiometer.hasChanged()) 
+  {
+    udp.broadcastTo(potentiometer.color.c_str(), 7000);
+
+  }
 }
